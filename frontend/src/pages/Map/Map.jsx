fdbed8b285614023cf4bee256e743b0ecbf0caf5
ItsMatDev <thierry.matthieu@live.fr>
2023-12-20 17:28:03 +0100

    added search bar + api for finding address

--- frontend/src/pages/Map/Map.jsx
+++ frontend/src/pages/Map/Map.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import "./Map.scss";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import plugGreen from "../../assets/plug-icon-green.png";
-import { Icon } from "leaflet";
+import { L, Icon } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
+import { PlaceKit } from "@placekit/autocomplete-react";
+import "@placekit/autocomplete-js/dist/placekit-autocomplete.css";
 
 function Map() {
   const markers = [
@@ -16,85 +18,94 @@ function Map() {
     { coord: [50.61685, 3.024687] },
     { coord: [50.61685, 3.024687] },
     { coord: [50.61685, 3.024687] },
-    // [3.024687, 50.61685]
-    // [3.024687, 50.61685]
-    // [3.024687, 50.61685]
-    // [3.024687, 50.61685]
-    // [3.024687, 50.61685]
-    // [3.024687, 50.61685]
     { coord: [50.620099, 3.083623] },
-    // [3.083623, 50.620099]
-    // [3.083623, 50.620099]
-    // [3.083623, 50.620099]
     { coord: [50.617371, 3.083473] },
-    // [3.083473, 50.617371]
-    // [3.083473, 50.617371]
-    // [3.083473, 50.617371]
     { coord: [50.630735, 3.034746] },
-    // [3.034746, 50.630735]
     { coord: [50.629868, 3.032864] },
-    // [3.032864, 50.629868]
-    // [3.032864, 50.629868]
-    // [3.032864, 50.629868]
     { coord: [50.6207475, 3.075673] },
     { coord: [50.625233, 3.123037] },
-    // [3.123037, 50.625233]
     { coord: [50.6353937, 3.0385727] },
-    // [3.0385727, 50.6353937]
-    // [3.0385727, 50.6353937]
-    // [3.0385727, 50.6353937]
     { coord: [50.6319701, 3.0798087] },
-    // [3.0798087, 50.6319701]
   ];
+
   const customIcon = new Icon({
     iconUrl: plugGreen,
     iconSize: [38, 38],
   });
-
-  const [myPosition, setMyPosition] = useState({
+  const map = useRef(null);
+  const [coords, setCoords] = useState({
     lat: 0,
     long: 0,
     accuracy: 0,
   });
 
   useEffect(() => {
+    if (coords && map.current) {
+      map.current.setView(coords.lat, coords.long, 13);
+    }
+  }, [coords, map]);
+
+  const handlePick = useCallback((_, item) => {
+    const [lat, lng] = item.coordinates.split(",");
+    setCoords([lat, lng]);
+  }, []);
+
+  const handleGeolocation = useCallback((_, pos) => {
+    setCoords([pos.coords.latitude, pos.coords.longitude]);
+  }, []);
+
+  useEffect(() => {
     const getPosition = (position) => {
       const lat = position.coords.latitude;
       const long = position.coords.longitude;
-      const accuracy = position.coords.accuracy;
+      const { accuracy } = position.coords;
       const currentPosition = { lat, long, accuracy };
-      setMyPosition(currentPosition);
+      setCoords({
+        lat: currentPosition.lat,
+        long: currentPosition.long,
+        accuracy: currentPosition.accuracy,
+      });
     };
 
     navigator.geolocation.getCurrentPosition(getPosition);
   }, []);
-  console.log(myPosition);
 
-  const hasValidPosition = myPosition.lat !== 0 && myPosition.long !== 0;
+  const hasValidPosition =
+    coords.lat !== 0 && coords.long !== 0 && coords.accuracy < 2000;
+
   return (
-    hasValidPosition && (
-      <MapContainer center={[myPosition.lat, myPosition.long]} zoom={13}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+    <>
+      <form role="search" className="searchBar">
+        <PlaceKit
+          apiKey={import.meta.env.VITE_API_KEY}
+          onPick={handlePick}
+          onGeolocation={handleGeolocation}
+          placeholder="Rechercher une adresse..."
         />
-        <MarkerClusterGroup>
-          {markers.map((marker) => (
-            <Marker position={marker.coord} icon={customIcon}>
-              <Popup>{marker.popUp}</Popup>
+      </form>
+      {hasValidPosition && (
+        <MapContainer ref={map} center={[coords.lat, coords.long]} zoom={13}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <MarkerClusterGroup>
+            {markers.map((marker) => (
+              <Marker position={marker.coord} icon={customIcon}>
+                <Popup>{marker.popUp}</Popup>
+              </Marker>
+            ))}
+          </MarkerClusterGroup>
+          {coords && (
+            <Marker position={[coords.lat, coords.long]}>
+              <Popup>
+                <h1>Vous êtes ici</h1>
+              </Popup>
             </Marker>
-          ))}
-          <Marker
-            position={[myPosition.lat, myPosition.long]}
-            icon={customIcon}
-          >
-            <Popup>
-              <h1>Ma position</h1>
-            </Popup>
-          </Marker>
-        </MarkerClusterGroup>
-      </MapContainer>
-    )
+          )}
+        </MapContainer>
+      )}
+    </>
   );
 }
 

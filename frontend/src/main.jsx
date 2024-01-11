c6b962eeebb93e455a3900c2f1e71bbebe081c69
JulienOza <julien.ozarowski@gmail.com>
2024-01-11 17:06:50 +0100

    adding EditProfile page and route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home/Home";
 import Map from "./pages/Map/Map";
 import Connection from "./pages/Connection/Connection";
 import Subscribe from "./pages/Subscribe/Subscribe";
+import EditProfile from "./pages/EditProfile/EditProfile";
 
 const router = createBrowserRouter([
   {
@@ -38,6 +39,10 @@ const router = createBrowserRouter([
   {
     path: "/subscribe",
     element: <Subscribe />,
+  },
+  {
+    path: "/editprofile",
+    element: <EditProfile />,
   },
 ]);
 

b523c605f9f321acc0ee75f1f52dc6273316f8e6
bravax <xavierlobez@hotmail.fr>
2024-01-24 11:41:26 +0100

    Amended

--- backend/src/models/BookAvailable.js
+++ backend/src/models/BookAvailable.js
@@ -6,8 +6,9 @@ class bookAvailable extends AbstractManager {
   }
 
   async getAll() {
-    const [rows] = await this.database.query(`SELECT date, charge_point_id
-      FROM ${this.table} `);
+    const [rows] = await this.database
+      .query(`SELECT date, charge_point_id, car_id
+    FROM ${this.table} `);
 
     return rows;
   }
